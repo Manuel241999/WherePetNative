5f6882f2da99d8e1731a12555c47395390eae405
Manuel241999 <mordonezsilva@gmail.com>
2024-05-09 16:27:25 -0600

    xd

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useRef, useState } from 'react';
-import { View, Platform, Image } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE, Polyline } from 'react-native-maps';
-import { Location } from '../../../infrastructure/interfaces/location';
-import { FAB } from '../ui/FAB';
-import { gql, useQuery } from '@apollo/client';
-=======
 import React, {useEffect, useRef, useState} from 'react';
 import {View, Platform, Image} from 'react-native';
 import MapView, {
@@ -17,16 +9,11 @@ import MapView, {
 import {Location} from '../../../infrastructure/interfaces/location';
 import {FAB} from '../ui/FAB';
 import {gql, useQuery} from '@apollo/client';
->>>>>>> Manuel
 
 // Definición del query de GraphQL
 const GET_LOCATION_DATA = gql`
   query ObtenerZonas {
-<<<<<<< HEAD
     obtenerZonas(input: { idUsuario: "6629b321d89f4e5852983afd" }) {
-=======
-    obtenerZonas(input: {idUsuario: "6629b321d89f4e5852983afd"}) {
->>>>>>> Manuel
       latitud
       longitud
     }
@@ -44,13 +31,8 @@ export const Map = ({showsUserLocation = false, initialLocation}: Props) => {
   const [isFollowingUser, setIsFollowingUser] = useState(true);
   const [showPath, setShowPath] = useState(true);
 
-<<<<<<< HEAD
   const { data, startPolling, stopPolling } = useQuery(GET_LOCATION_DATA, {
     fetchPolicy: 'network-only'
-=======
-  const {data, startPolling, stopPolling} = useQuery(GET_LOCATION_DATA, {
-    fetchPolicy: 'network-only',
->>>>>>> Manuel
   });
 
   useEffect(() => {
@@ -61,20 +43,10 @@ export const Map = ({showsUserLocation = false, initialLocation}: Props) => {
   useEffect(() => {
     if (data && data.obtenerZonas && data.obtenerZonas.length > 0) {
       const lastLocation = data.obtenerZonas[data.obtenerZonas.length - 1];
-<<<<<<< HEAD
       setLocationHistory(prev => [...prev, {
         latitude: lastLocation.latitud,
         longitude: lastLocation.longitud
       }]);
-=======
-      setLocationHistory(prev => [
-        ...prev,
-        {
-          latitude: lastLocation.latitud,
-          longitude: lastLocation.longitud,
-        },
-      ]);
->>>>>>> Manuel
     }
   }, [data]);
 
@@ -95,45 +67,12 @@ export const Map = ({showsUserLocation = false, initialLocation}: Props) => {
         ref={mapRef}
         showsUserLocation={showsUserLocation}
         provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE}
-<<<<<<< HEAD
-        style={{ flex: 1 }}
-=======
         style={{flex: 1}}
->>>>>>> Manuel
         initialRegion={{
           latitude: initialLocation.latitude,
           longitude: initialLocation.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-<<<<<<< HEAD
-        }}
-      >
-        <Marker
-          coordinate={locationHistory[locationHistory.length - 1]}
-          title="Current Location"
-        >
-          <Image
-            source={require("../../../assets/perro.png")}
-            style={{ width: 50, height: 50 }}
-          />
-        </Marker>
-        {showPath && <Polyline
-          coordinates={locationHistory}
-          strokeColor="red"
-          strokeWidth={5}
-        />}
-      </MapView>
-
-      <FAB
-        iconName='eye-outline'
-        onPress={() => setShowPath(!showPath)}
-        style={{ bottom: 80, right: 20 }}
-      />
-      <FAB
-        iconName='compass-outline'
-        onPress={centerOnPet}
-        style={{ bottom: 20, right: 20 }}
-=======
         }}>
         <Marker
           coordinate={locationHistory[locationHistory.length - 1]}
@@ -167,7 +106,6 @@ export const Map = ({showsUserLocation = false, initialLocation}: Props) => {
         iconName="compass-outline"
         onPress={centerOnPet}
         style={{bottom: 20, right: 20}}
->>>>>>> Manuel
       />
     </>
   );
